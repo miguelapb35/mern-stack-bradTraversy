e812729081ac35187012cff448a9009c5f0c322e
nicholasnbg <nicholasnbg@gmail.com>
2018-06-25 20:44:53 +0700

    Added AddEducation component along with redux functionality

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import CreateProfile from "./components/create-profile/CreateProfile";
 import "./App.css";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -78,6 +79,13 @@ class App extends Component {
                   exact
                   path="/add-experience"
                   component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
                 />
               </Switch>
             </div>
